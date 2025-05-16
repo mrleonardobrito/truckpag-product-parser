eaf5ad50a8a54ac767181434535d41bc3ea1fd0b
mrleonardobrito <lbritogit@gmail.com>
2025-05-16 14:20:01 -0300

    creating initial structure

---
+++ app/Models/Product.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
+
+class Product extends Model {
+    protected $fillable = [
+        'code',
+        'status',
+        'imported_t',
+        'url',
+        'creator',
+        'created_t',
+        'last_modified_t',
+        'product_name',
+        'quantity',
+        'brands',
+        'categories',
+        'labels',
+        'cities',
+        'purchase_places',
+        'stores',
+        'ingredients_text',
+        'traces',
+        'serving_size',
+        'serving_quantity',
+        'nutriscore_score',
+        'nutriscore_grade',
+        'main_category',
+        'image_url',
+    ];
+
+    protected function casts() {
+        return [
+            'imported_t' => 'datetime',
+            'created_t' => 'datetime',
+            'last_modified_t' => 'datetime',
+        ];
+    }
+}
---
+++ config/database.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Str;
+
+return [
+    'default' => env('DB_CONNECTION', 'mongodb'),
+    'connections' => [
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'dsn' => env('MONGODB_URI', 'mongodb://localhost:27017'),
+            'database' => env('MONGODB_DATABASE', 'laravel_app'),
+        ],
+    ],
+
+    'migrations' => [
+        'table' => 'migrations',
+        'update_date_on_publish' => true,
+    ],
+
+    'redis' => [
+        'client' => env('REDIS_CLIENT', 'phpredis'),
+
+        'cache' => [
+            'url' => env('REDIS_URL'),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'username' => env('REDIS_USERNAME'),
+            'password' => env('REDIS_PASSWORD'),
+            'port' => env('REDIS_PORT', '6379'),
+            'database' => env('REDIS_CACHE_DB', '1'),
+        ],
+
+        'session' => [
+            'url' => env('REDIS_URL'),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'username' => env('REDIS_USERNAME'),
+            'password' => env('REDIS_PASSWORD'),
+            'port' => env('REDIS_PORT', '6379'),
+            'database' => env('REDIS_SESSION_DB', '2'),
+        ]
+
+    ],
+
+];
